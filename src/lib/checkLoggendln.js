8c8b32082c330cc6eb8d43f0c6bc32abd840e21d
nakhun9279 <fjfj9279@naver.com>
2020-09-10 23:07:51 +0900

    23.5.3 로그인했을때만 API를 사용할수 있게하기

---
+++ src/lib/checkLoggendln.js
@@ -0,0 +1,12 @@
+const checkLoggedIn = (ctx, next) => {
+    if (!ctx.state.user) {
+      ctx.status = 401; // Unauthorized
+      return;
+    }
+    return next();
+  };
+  
+  
+  export default checkLoggedIn;
+  
+  
