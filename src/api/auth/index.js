c8eb22bf0bf7e3f5cd5636a64e80722483407797
nakhun9279 <fjfj9279@naver.com>
2020-09-10 22:21:39 +0900

    23.3.2 로그인 구현하기

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,8 +2,8 @@ import Router from 'koa-router';
 import * as authCtrl from './auth.ctrl';
 const auth = new Router();
 
-auth.post('/register', authCtrl.register);
-//auth.post('/login', authCtrl.login);
+//auth.post('/register', authCtrl.register);
+auth.post('/login', authCtrl.login);
 //auth.get('/check', authCtrl.check);
 //auth.post('/logout', authCtrl.logout);
 
