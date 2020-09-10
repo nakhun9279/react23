43239a1cfd405d7212b9d90d4e5fb1a6505b5ca4
nakhun9279 <fjfj9279@naver.com>
2020-09-10 14:15:51 +0900

    22.5.2 모델 생성

--- src/models/post.js
+++ src/models/post.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 
+
 const { Schema } = mongoose;
+
+
 
 const PostSchema = new Schema({
   title: String,
@@ -11,3 +14,8 @@ const PostSchema = new Schema({
     default: Date.now, // 현재 날짜를 기본값으로 지정
   },
 });
+
+
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
