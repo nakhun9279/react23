bf63a875503b567019689873b16e862b88256e59
nakhun9279 <fjfj9279@naver.com>
2020-09-10 14:12:02 +0900

    22.4.1 기존코드 ES Module 형태로 바꾸기

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+
+import api from './api';
+
 
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT,  MONGO_URI } = process.env;
+const { PORT, MONGO_URI } = process.env;
 
 mongoose
 .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
@@ -17,7 +21,6 @@ mongoose
     console.error(e);
 });
 
-const api = require('./api');
 
 
 
